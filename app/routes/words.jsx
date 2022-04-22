b754124ab25a6b629d14b4c4c53fbc3ed162f5f9
Harsh Singh <hi.harsh@protonmail.ch>
2022-04-22 14:30:36 -0400

    Styled words list

--- app/routes/words.jsx
+++ app/routes/words.jsx
@@ -2,10 +2,10 @@ import { Link, Outlet } from "remix";
 
 export default function WordsRoute() {
   return (
-    <div>
-      <h1>Japanese Vocabulary</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold my-10">Japanese Vocabulary</h1>
       <Outlet />
-      <Link to="/words/new">Add a new word</Link>
+      <Link to="/words/new" className="bg-blue-200 hover:bg-blue-100 transition-colors text-white py-4 px-10 text-xl rounded-lg my-20 shadow-button select-none">Add a new word</Link>
     </div>
   );
 }
